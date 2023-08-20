d0e206d964375ffde174f1b9883585bf48975dcb
Romulo de Oliveira <devromulodeoliveira@gmail.com>
2023-08-20 15:58:09 -0300

    desenvolvimento inicial do model Address

---
+++ Models/AddressModel.cs
@@ -0,0 +1,16 @@
+namespace BlogAPIDotnet6.Models;
+
+public class AddressModel
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public string? City { get; set; }
+    public string? State { get; set; }
+    public string? Country { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime? UpdatedAt { get; set; }
+
+    public AddressModel()
+    {
+        CreatedAt = DateTime.UtcNow;
+    }
+}
