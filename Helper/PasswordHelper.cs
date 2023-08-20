537317d4999ba63143d7ebaa3898cd47501a51f3
Romulo de Oliveira <devromulodeoliveira@gmail.com>
2023-08-20 19:03:47 -0300

    correção do modificador de acesso do metodo CreatePasswordHash

--- Helper/PasswordHelper.cs
+++ Helper/PasswordHelper.cs
@@ -4,7 +4,7 @@ namespace BlogAPIDotnet6.Helper;
 
 public class PasswordHelper
 {
-    private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+    public void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
     {
         using (var hmac = new HMACSHA512())
         {
