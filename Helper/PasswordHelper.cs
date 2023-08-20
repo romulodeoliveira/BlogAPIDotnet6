fdfe87c73c183936f976452543897825804f439e
Romulo de Oliveira <devromulodeoliveira@gmail.com>
2023-08-20 18:39:37 -0300

    desenvolvimento do metodo de criação do hash da senha do usuário

---
+++ Helper/PasswordHelper.cs
@@ -0,0 +1,15 @@
+using System.Security.Cryptography;
+
+namespace BlogAPIDotnet6.Helper;
+
+public class PasswordHelper
+{
+    private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+    {
+        using (var hmac = new HMACSHA512())
+        {
+            passwordSalt = hmac.Key;
+            passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+        }
+    }
+}
