490967e98af3662d977dea42cbd01face970f7bd
Romulo de Oliveira <devromulodeoliveira@gmail.com>
2023-08-20 16:30:59 -0300

    adicionando as configurações de acesso ao banco de dados

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using BlogAPIDotnet6.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+string dbConfig = "Server=localhost;Port=3306;Database=blogtechapi;Uid=arch;Pwd=1234;";
+builder.Services.AddDbContextPool<DataContext>(options => options.UseMySql(dbConfig, ServerVersion.AutoDetect(dbConfig)));
 
 var app = builder.Build();
 
