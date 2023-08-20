3a72d96e26040e1820916d876c052949dfec5675
Romulo de Oliveira <devromulodeoliveira@gmail.com>
2023-08-20 16:59:07 -0300

    adição dos metodos de password

--- Models/UserModel.cs
+++ Models/UserModel.cs
@@ -5,6 +5,8 @@ public class UserModel
     public Guid Id { get; set; } = Guid.NewGuid();
     public string Username { get; set; }
     public string Email { get; set; }
+    public byte[] PasswordHash { get; set; }
+    public byte[] PasswordSalt { get; set; }
     public string? Firstname { get; set; }
     public string? Lastname { get; set; }
     public DateTime CreatedAt { get; set; }
