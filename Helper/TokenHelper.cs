703d4d918545caa8215643577f35fca81efd0586
Romulo de Oliveira <devromulodeoliveira@gmail.com>
2023-08-21 00:26:59 -0300

    mudança do nivel de acesso a Key de private para public

--- Helper/TokenHelper.cs
+++ Helper/TokenHelper.cs
@@ -9,7 +9,7 @@ public class TokenHelper
 {
     // https://jwt.io/
     
-    private string Key = "uBMbQErWn&hP7mW5TKa9WisaxTU4EgvG$ZWoFj92Te#NvQmB!Xag$Emzb&eQc7zSkVxWkkb7G&dt9GNpENx7HJfsb*&DHyQvkJW6*e*fvDkrHPT@cem49@*&HRKLGp$e";
+    public string Key = "uBMbQErWn&hP7mW5TKa9WisaxTU4EgvG$ZWoFj92Te#NvQmB!Xag$Emzb&eQc7zSkVxWkkb7G&dt9GNpENx7HJfsb*&DHyQvkJW6*e*fvDkrHPT@cem49@*&HRKLGp$e";
     public string CreateToken(UserModel user, IConfiguration configuration)
     {
         List<Claim> claims = new List<Claim>
