f0a343e32b120eedff8700f9d340fc9608175ac3
Romulo de Oliveira <devromulodeoliveira@gmail.com>
2023-08-21 00:11:57 -0300

    criação do metodo GetUserByUsername na interface de usuários

--- Repositories/Interfaces/IUserRepository.cs
+++ Repositories/Interfaces/IUserRepository.cs
@@ -5,6 +5,7 @@ namespace BlogAPIDotnet6.Repositories.Interfaces;
 public interface IUserRepository
 {
     UserModel ListUserById(Guid id);
+    UserModel GetUserByUsername(string username);
     List<UserModel> GetAllUsers();
     UserModel AddUser(UserModel user);
     UserModel UpdateUser(UserModel user);
