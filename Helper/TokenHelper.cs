18c56463799c8e54ba564db68e51b1e60a91d0f2
Romulo de Oliveira <devromulodeoliveira@gmail.com>
2023-08-21 00:10:40 -0300

    desenvolvimento do helper de token com a chave de segurança e o metodo CreateToken

---
+++ Helper/TokenHelper.cs
@@ -0,0 +1,34 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using BlogAPIDotnet6.Models;
+using Microsoft.IdentityModel.Tokens;
+
+namespace BlogAPIDotnet6.Helper;
+
+public class TokenHelper
+{
+    // https://jwt.io/
+    
+    private string Key = "uBMbQErWn&hP7mW5TKa9WisaxTU4EgvG$ZWoFj92Te#NvQmB!Xag$Emzb&eQc7zSkVxWkkb7G&dt9GNpENx7HJfsb*&DHyQvkJW6*e*fvDkrHPT@cem49@*&HRKLGp$e";
+    public string CreateToken(UserModel user, IConfiguration configuration)
+    {
+        List<Claim> claims = new List<Claim>
+        {
+            new Claim(ClaimTypes.Name, user.Username),
+            new Claim(ClaimTypes.Role, "Admin") // EXEMPLO DE COMO USAR ROLES!!! 
+        };
+
+        var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(Key));
+
+        var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+        var token = new JwtSecurityToken(
+            claims: claims,
+            expires: DateTime.Now.AddDays(1),
+            signingCredentials: creds);
+
+        var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+
+        return jwt;
+    }
+}
