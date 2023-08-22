6f98c10b8fc47ce1a16232cee7ec4d94ef54ece6
Romulo de Oliveira <devromulodeoliveira@gmail.com>
2023-08-22 00:10:45 -0300

    adição da coluna Role para guardar o nivel de permissão do usuário

--- Models/UserModel.cs
+++ Models/UserModel.cs
@@ -9,6 +9,7 @@ public class UserModel
     public byte[] PasswordSalt { get; set; }
     public string? Firstname { get; set; }
     public string? Lastname { get; set; }
+    public string Role { get; set; }
     public DateTime CreatedAt { get; set; }
     public DateTime? UpdatedAt { get; set; }
 
