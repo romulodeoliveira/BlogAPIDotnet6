70e791c34a6a23fc667234b06fcb1464af2ae611
Romulo de Oliveira <devromulodeoliveira@gmail.com>
2023-08-22 00:46:49 -0300

    removendo da interface o metodo FindUserById e passando como parametro no metodo DeleteUser uma string

--- Repositories/Interfaces/IUserRepository.cs
+++ Repositories/Interfaces/IUserRepository.cs
@@ -4,10 +4,9 @@ namespace BlogAPIDotnet6.Repositories.Interfaces;
 
 public interface IUserRepository
 {
-    UserModel ListUserById(Guid id);
     UserModel GetUserByUsername(string username);
     List<UserModel> GetAllUsers();
     UserModel AddUser(UserModel user);
     UserModel UpdateUser(UserModel user);
-    bool DeleteUser(Guid id);
+    bool DeleteUser(string username);
 }
