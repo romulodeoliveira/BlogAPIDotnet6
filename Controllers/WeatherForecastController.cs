489fa3884bcaa68a493a675dab10df56626d2132
Romulo de Oliveira <devromulodeoliveira@gmail.com>
2023-08-22 00:14:38 -0300

    atualização do role de permissão usando a Classe Roles

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -1,3 +1,4 @@
+using BlogAPIDotnet6.Helper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -19,7 +20,7 @@ public class WeatherForecastController : ControllerBase
         _logger = logger;
     }
 
-    [HttpGet(Name = "GetWeatherForecast"), Authorize(Roles = "Admin")]
+    [HttpGet(Name = "GetWeatherForecast"), Authorize(Roles = Roles.Admin)]
     public IEnumerable<WeatherForecast> Get()
     {
         return Enumerable.Range(1, 5).Select(index => new WeatherForecast
