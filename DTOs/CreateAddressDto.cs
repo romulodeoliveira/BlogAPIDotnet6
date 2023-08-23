b1a0b80c316c2ac1bb9ca3815499682d56f3bb38
Romulo de Oliveira <devromulodeoliveira@gmail.com>
2023-08-23 00:44:48 -0300

    desenvolvendo o DTO para criação de endereços

---
+++ DTOs/CreateAddressDto.cs
@@ -0,0 +1,8 @@
+namespace BlogAPIDotnet6.DTOs;
+
+public class CreateAddressDto
+{
+    public string City { get; set; }
+    public string State { get; set; }
+    public string Country { get; set; }
+}
