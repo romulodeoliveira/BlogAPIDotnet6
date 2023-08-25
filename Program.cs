7b3aabe9b67bc93e4664aca06485547e751bc3d6
Romulo de Oliveira <devromulodeoliveira@gmail.com>
2023-08-24 23:54:05 -0300

    configuração do sistema de serialização json do aplicativo

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using System.Text;
+using System.Text.Json.Serialization;
 using BlogAPIDotnet6.Data;
 using BlogAPIDotnet6.Helper;
 using BlogAPIDotnet6.Repositories.Implementations;
@@ -14,6 +15,10 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddControllers().AddJsonOptions(options =>
+{
+    options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.Preserve;
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(options =>
