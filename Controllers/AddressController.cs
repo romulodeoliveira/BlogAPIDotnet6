3f98329c01f7a893b288e9fd5fe745956d8e5dbd
Romulo de Oliveira <devromulodeoliveira@gmail.com>
2023-08-24 23:53:05 -0300

    Desenvolvimento da função de criação de endereços

--- Controllers/AddressController.cs
+++ Controllers/AddressController.cs
@@ -1,3 +1,6 @@
+using System.Security.Claims;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using BlogAPIDotnet6.DTOs;
 using BlogAPIDotnet6.Models;
 using BlogAPIDotnet6.Repositories.Interfaces;
@@ -11,10 +14,12 @@ namespace BlogAPIDotnet6.Controllers;
 public class AddressController : ControllerBase
 {
     private readonly IAddressRepository _addressRepository;
+    private readonly IUserRepository _userRepository;
 
-    public AddressController(IAddressRepository addressRepository)
+    public AddressController(IAddressRepository addressRepository, IUserRepository userRepository)
     {
         _addressRepository = addressRepository;
+        _userRepository = userRepository;
     }
 
     [Authorize]
@@ -37,20 +42,36 @@ public class AddressController : ControllerBase
             {
                 address.City = request.City;
             }
-            
+
             if (!string.IsNullOrEmpty(request.State))
             {
                 address.State = request.State;
             }
-            
+
             if (!string.IsNullOrEmpty(request.Country))
             {
                 address.Country = request.Country;
             }
-
-            _addressRepository.AddAddress(address);
             
-            return Ok(address);
+            var username = User.Identity.Name;
+
+            var user = _userRepository.GetUserByUsername(username);
+            if (user != null)
+            {
+                address.Username = username;
+                address.User = user;
+                _addressRepository.AddAddress(address);
+                
+                user.AddressId = address.Id;
+                user.Address = address;
+                _userRepository.UpdateUser(user);
+
+                return Ok(address);
+            }
+            else
+            {
+                return BadRequest("Usuário não encontrado.");
+            }
         }
         catch (System.Exception error)
         {
