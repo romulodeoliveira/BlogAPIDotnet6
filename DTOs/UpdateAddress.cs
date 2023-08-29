739d5494e60544ec4b97d2ae777c5a075b15ef5d
Romulo de Oliveira <devromulodeoliveira@gmail.com>
2023-08-28 23:40:07 -0300

    desenvolimento do DTO para atualização de endereços

---
+++ DTOs/UpdateAddress.cs
@@ -0,0 +1,9 @@
+namespace BlogAPIDotnet6.DTOs;
+
+public class UpdateAddress
+{
+    public string? City { get; set; }
+    public string? State { get; set; }
+    public string? Country { get; set; }
+    public string Username { get; set; }
+}
