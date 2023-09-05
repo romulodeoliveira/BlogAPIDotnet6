fc930dac64d848524c397b6f902e019bd8da056e
Romulo de Oliveira <devromulodeoliveira@gmail.com>
2023-09-05 00:47:42 -0300

    implementação da interface para posts

---
+++ Repositories/Implementations/PostRepository.cs
@@ -0,0 +1,70 @@
+using BlogAPIDotnet6.Data;
+using BlogAPIDotnet6.Exceptions;
+using BlogAPIDotnet6.Models;
+using BlogAPIDotnet6.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlogAPIDotnet6.Repositories.Implementations;
+
+public class PostRepository : IPostRepository
+{
+    private readonly DataContext _dataContext;
+
+    public PostRepository(DataContext dataContext)
+    {
+        _dataContext = dataContext;
+    }
+    
+    public PostModel GetPostById(Guid id)
+    {
+        return _dataContext.Posts.FirstOrDefault(x => x.Id == id);
+    }
+
+    public List<PostModel> GetAllPosts()
+    {
+        return _dataContext.Posts.ToList();
+    }
+
+    public PostModel AddPost(PostModel post)
+    {
+        _dataContext.Posts.Add(post);
+        _dataContext.SaveChanges();
+
+        return post;
+    }
+
+    public PostModel UpdatePost(PostModel post)
+    {
+        _dataContext.Entry(post).State = EntityState.Modified;
+        _dataContext.SaveChanges();
+
+        return post;
+    }
+
+    public bool DeleteAddress(Guid id)
+    {
+        if (id == Guid.Empty)
+        {
+            throw new ArgumentException("O ID fornecido não é válido.", nameof(id));
+        }
+
+        PostModel post = GetPostById(id);
+
+        if (post == null)
+        {
+            throw new PostNotFoundException("A postagem não foi encontrada.");
+        }
+
+        try
+        {
+            _dataContext.Posts.Remove(post);
+            _dataContext.SaveChanges();
+
+            return true;
+        }
+        catch (DbUpdateException error)
+        {
+            throw new DataAccessException($"Ocorreu um erro ao excluir a postagem no banco de dados. {error}");
+        }
+    }
+}
