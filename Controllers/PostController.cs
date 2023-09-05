8c2682e2bf519c25a556a733d606ad82b2fe9c87
Romulo de Oliveira <devromulodeoliveira@gmail.com>
2023-09-05 01:30:57 -0300

    desenvolvimento da função para listar postagens

--- Controllers/PostController.cs
+++ Controllers/PostController.cs
@@ -1,3 +1,4 @@
+using BlogAPIDotnet6.Repositories.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BlogAPIDotnet6.Controllers;
@@ -6,5 +7,27 @@ namespace BlogAPIDotnet6.Controllers;
 [Route("api/[controller]")]
 public class PostController : ControllerBase
 {
-    
+    private readonly IPostRepository _postRepository;
+
+    public PostController(IPostRepository postRepository)
+    {
+        _postRepository = postRepository;
+    }
+
+    [HttpGet("list-posts")]
+    public IActionResult ListPosts()
+    {
+        try
+        {
+            var posts = _postRepository.GetAllPosts()
+                .OrderByDescending(p => p.CreatedAt)
+                .ToList();
+
+            return Ok(posts);
+        }
+        catch (Exception error)
+        {
+            return StatusCode(500, $"Erro interno do servidor: {error.Message}");
+        }
+    }
 }
