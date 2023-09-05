ff64777c582a35f64efc1b8cbf14ab73f52a096b
Romulo de Oliveira <devromulodeoliveira@gmail.com>
2023-09-05 00:47:27 -0300

    criação da interface para posts

---
+++ Repositories/Interfaces/IPostRepository.cs
@@ -0,0 +1,12 @@
+using BlogAPIDotnet6.Models;
+
+namespace BlogAPIDotnet6.Repositories.Interfaces;
+
+public interface IPostRepository
+{
+    PostModel GetPostById(Guid id);
+    List<PostModel> GetAllPosts();
+    PostModel AddPost(PostModel post);
+    PostModel UpdatePost(PostModel post);
+    bool DeleteAddress(Guid id);
+}
