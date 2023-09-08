52e52a920ad89c35d27e49821a7237511335b121
Romulo de Oliveira <devromulodeoliveira@gmail.com>
2023-09-07 22:19:49 -0300

    criando o metodo do model post para relação entre varios posts para um usuário

--- Models/PostModel.cs
+++ Models/PostModel.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace BlogAPIDotnet6.Models;
 
 public class PostModel
@@ -5,6 +7,9 @@ public class PostModel
     public Guid Id { get; set; } = Guid.NewGuid();
     public string Title { get; set; }
     public string Body { get; set; }
+    public string Username { get; set; }
+    [ForeignKey(nameof(Username))]
+    public virtual UserModel User { get; set; }
     public DateTime CreatedAt { get; set; }
     public DateTime? UpdatedAt { get; set; }
 
