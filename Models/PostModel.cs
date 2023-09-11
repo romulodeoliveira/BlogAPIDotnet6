a88ede2b3e1f952954586617e71c5f185f3b87ad
Romulo de Oliveira <devromulodeoliveira@gmail.com>
2023-09-10 22:37:01 -0300

    alteração da ordem dos metodos do model

--- Models/PostModel.cs
+++ Models/PostModel.cs
@@ -8,10 +8,9 @@ public class PostModel
     public Guid Id { get; set; } = Guid.NewGuid();
     public string Title { get; set; }
     public string Body { get; set; }
-    public string Username { get; set; }
     public bool IsPublished { get; set; }
-    [Index(IsUnique = true)]
     public string Slug { get; set; }
+    public string Username { get; set; }
     [ForeignKey(nameof(Username))]
     public virtual UserModel User { get; set; }
     public DateTime CreatedAt { get; set; }
