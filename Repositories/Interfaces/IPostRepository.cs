f9f23510da6b37561a14dc4563d5b4eaa4c981b9
Romulo de Oliveira <devromulodeoliveira@gmail.com>
2023-09-13 00:26:38 -0300

    criação do metodo booleano IsSlugUnique na interface de post

--- Repositories/Interfaces/IPostRepository.cs
+++ Repositories/Interfaces/IPostRepository.cs
@@ -7,6 +7,7 @@ public interface IPostRepository
     PostModel GetPostById(Guid id);
     List<PostModel> GetAllPosts();
     PostModel AddPost(PostModel post);
+    bool IsSlugUnique(string slug);
     PostModel UpdatePost(PostModel post);
     bool DeletePost(Guid id);
 }
