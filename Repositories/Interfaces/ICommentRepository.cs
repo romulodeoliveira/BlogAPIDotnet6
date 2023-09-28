0f8b38fd1a79206eeb4cb72be60f2b808a882d5a
Romulo de Oliveira <devromulodeoliveira@gmail.com>
2023-09-28 00:51:33 -0300

    alteração do nome do metodo para criar comentarios

--- Repositories/Interfaces/ICommentRepository.cs
+++ Repositories/Interfaces/ICommentRepository.cs
@@ -6,7 +6,7 @@ public interface ICommentRepository
 {
     CommentModel GetCommentById(Guid id);
     List<CommentModel> GetAllCommentsForPublication(Guid id);
-    CommentModel AddAddress(CommentModel comment);
+    CommentModel AddComment(CommentModel comment);
     CommentModel UpdateComment(CommentModel comment);
     bool DeleteComment(Guid id);
 }
