a97e95bb7495cd0eecb93fa13f8d01c9a82c627e
Romulo de Oliveira <devromulodeoliveira@gmail.com>
2023-09-28 00:52:21 -0300

    exclusão do metodo title

--- Models/CommentModel.cs
+++ Models/CommentModel.cs
@@ -5,7 +5,6 @@ namespace BlogAPIDotnet6.Models;
 public class CommentModel
 {
     public Guid Id { get; set; } = Guid.NewGuid();
-    public string Title { get; set; }
     public string Body { get; set; }
     public DateTime CreatedAt { get; set; }
     public DateTime? UpdatedAt { get; set; }
