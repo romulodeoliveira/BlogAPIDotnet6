973f6ec89c62ca4decf113b86795e55177f7fe29
Romulo de Oliveira <devromulodeoliveira@gmail.com>
2023-09-30 13:05:54 -0300

    criação do metodo para listar comentarios vinculados ao usuário

--- Repositories/Interfaces/ICommentRepository.cs
+++ Repositories/Interfaces/ICommentRepository.cs
@@ -6,6 +6,7 @@ public interface ICommentRepository
 {
     CommentModel GetCommentById(Guid id);
     List<CommentModel> GetAllCommentsForPublication(Guid id);
+    List<CommentModel> GetAllCommentsForUser(string username);
     CommentModel AddComment(CommentModel comment);
     CommentModel UpdateComment(CommentModel comment);
     bool DeleteComment(Guid id);
