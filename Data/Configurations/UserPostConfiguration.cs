07a93eb8c2ed71ab8e25bbb3a5f9e4cca7a43397
Romulo de Oliveira <devromulodeoliveira@gmail.com>
2023-10-01 18:13:11 -0300

    desenvolvimento da configuração relacionando usuário e post do data context

---
+++ Data/Configurations/UserPostConfiguration.cs
@@ -0,0 +1,16 @@
+using BlogAPIDotnet6.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BlogAPIDotnet6.Data.Configurations;
+
+public class UserPostConfiguration : IEntityTypeConfiguration<UserModel>
+{
+    public void Configure(EntityTypeBuilder<UserModel> builder)
+    {
+        builder
+            .HasMany(u => u.Posts) // um usuário tem muitos posts
+            .WithOne(p => p.User) // um post pertence a um usuário
+            .HasForeignKey(p => p.Username); // chave estrangeira em PostModel que liga a UserModel
+    }
+}
--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -17,5 +17,6 @@ public class DataContext : DbContext
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.ApplyConfiguration(new UserAddressConfiguration());
+        modelBuilder.ApplyConfiguration(new UserPostConfiguration());
     }
 }
