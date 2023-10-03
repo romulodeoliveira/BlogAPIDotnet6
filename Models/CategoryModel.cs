e601d2413a57290ecb486ac22e26a8ef6b05cd42
Romulo de Oliveira <devromulodeoliveira@gmail.com>
2023-10-03 02:03:07 -0300

    configuração da relação de muitos para um entre usuários e cateogira

--- Models/CategoryModel.cs
+++ Models/CategoryModel.cs
@@ -1,9 +1,14 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace BlogAPIDotnet6.Models;
 
 public class CategoryModel
 {
     public Guid Id { get; set; } = Guid.NewGuid();
     public string Title { get; set; }
+    public string Username { get; set; }
+    [ForeignKey(nameof(Username))]
+    public virtual UserModel User { get; set; }
     public DateTime CreatedAt { get; set; }
     public DateTime? UpdatedAt { get; set; }
 
--- Models/UserModel.cs
+++ Models/UserModel.cs
@@ -19,6 +19,7 @@ public class UserModel
     public virtual AddressModel? Address { get; set; }
     public ICollection<PostModel>? Posts { get; set; }
     public ICollection<CommentModel>? Comments { get; set; }
+    public ICollection<CategoryModel>? Categories { get; set; }
     public DateTime CreatedAt { get; set; }
     public DateTime? UpdatedAt { get; set; }
 
