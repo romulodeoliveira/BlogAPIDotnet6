7578c25375496b9386c22a710ab35c27b5c52dd7
Romulo de Oliveira <devromulodeoliveira@gmail.com>
2023-10-05 00:36:41 -0300

    tornando a coleção de posts opcional

--- Models/CategoryModel.cs
+++ Models/CategoryModel.cs
@@ -9,6 +9,7 @@ public class CategoryModel
     public string Username { get; set; }
     [ForeignKey(nameof(Username))]
     public virtual UserModel User { get; set; }
+    public ICollection<PostModel>? Posts { get; set; }
     public DateTime CreatedAt { get; set; }
     public DateTime? UpdatedAt { get; set; }
 
