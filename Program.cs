30aee24e384a45a86184a4091a2cac4e8cc1726c
Romulo de Oliveira <devromulodeoliveira@gmail.com>
2023-10-06 02:03:26 -0300

    tornando o swagger publico

--- Program.cs
+++ Program.cs
@@ -57,11 +57,8 @@ builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
