fddcae7178548e37a4809cd96ecc6eab4cfef732
Romulo de Oliveira <devromulodeoliveira@gmail.com>
2023-10-11 02:53:59 -0300

    retirada do metodo user

--- DTOs/Address/UpdateAddressDto.cs
+++ DTOs/Address/UpdateAddressDto.cs
@@ -5,5 +5,4 @@ public class UpdateAddressDto
     public string? City { get; set; }
     public string? State { get; set; }
     public string? Country { get; set; }
-    public string Username { get; set; }
 }
